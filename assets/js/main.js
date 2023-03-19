af104ea386078a1ebc9dd3cc9f2e0a8dfbf80c68
Ahmed Fouda <84880406+a7medfouda@users.noreply.github.com>
2023-03-19 20:49:03 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,8 @@ window.addEventListener("scroll", function () {
   }
 });
 // --------------
+// preloader
+let loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
